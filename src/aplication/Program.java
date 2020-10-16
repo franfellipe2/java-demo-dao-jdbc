b4c5a8c84da106796c78e61f6998219e61bfd3af
Francisco S. Filho <franfellipe2@hotmail.com>
2020-10-16 15:14:00 -0300

    Create Class Seller

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,11 +1,16 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
-		
+		Department dp = new Department(1, "Eletronicos");
+		Seller seller = new Seller(21, 2, "Bob", "bob@gmail.com", new Date(), 2000.00, dp);
+		System.out.println(seller);
 	}
 
 }
